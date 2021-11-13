dc3815c2275035651e68857fb27579c932a93490
Vishnu <vishnuudai@gmail.com>
2021-11-13 09:36:39 +0530

    Added Liscence Banners to src

--- app/src/main/java/com/codesocio/dofoo/HelperClasses/GeoLocation.java
+++ app/src/main/java/com/codesocio/dofoo/HelperClasses/GeoLocation.java
@@ -1,4 +1,23 @@
-package com.codesocio.dofoo.HelperClasses;
+ /*
+  * DoFoo - A donation application
+  * Copyright (C) 2021  CodeSocio
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  * Contact us at --> codesociodevs@gmail.com
+  */
+
+ package com.codesocio.dofoo.HelperClasses;
 /*
  * Equation for setting for radius based search
  * acos(sin(curLat) * sin(Lat) + cos(CurLat) * cos(Lat) * cos(Lon - (curLong))) * 6371 <= searchRadius;
